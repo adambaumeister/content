18cb0e847cd833c3d685252b66a25d86d9624ad8
Adam Baumeister <abigserve@gmail.com>
2020-10-25 14:55:40 +1100

    Add SendReminderSMS.py

---
+++ Packs/Talarix/Scripts/SendReminderSMS/SendReminderSMS.py
@@ -0,0 +1,38 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+
+''' IMPORTS '''
+CONTEXT_KEY = "ReminderCount"
+
+
+def get_context():
+    inc = demisto.incidents()[0]
+    res = demisto.executeCommand("getContext", {"id": inc["id"]})
+    try:
+        return res[0]['Contents'].get('context') or {}
+    except Exception:
+        return {}
+
+
+def send_and_update(context):
+    message = demisto.args().get("message")
+
+    c = 0
+    if CONTEXT_KEY in context:
+        c = context[CONTEXT_KEY]
+
+    c = c + 1
+    message = message.format(c)
+
+    res = demisto.executeCommand("TalarixSendMessage", {
+        "message": message,
+        "number": demisto.args().get("number")
+    })
+
+    demisto.executeCommand("Set", {"key": CONTEXT_KEY, "value": c})
+    return res
+
+
+context = get_context()
+demisto.results(send_and_update(context))
